b13233643f6e439630c7e87c3a877bf1719fddd2
allistermugaisi <allistermugaisi@gmail.com>
2023-03-08 15:54:58 +0300

    Completed core project structure Handled file & folder structure of the project Added style sheet css file to create intuitive UI/UX of the frontend Created components that handles QuestionBox & Result from each questions Initialized and setup react project using vite

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react';
+import { QuestionBox, Result } from './components';
+
+// Mocked backend service
+import quizService from './quizService';
+
+function App() {
+	const [score, setScore] = useState(0);
+	const [responses, setResponses] = useState(0);
+	const [questionBank, setQuestionBank] = useState([]);
+
+	return (
+		<div className="container">
+			<div className="title">QuizBee</div>
+			{/* {this.state.questionBank.length > 0 &&
+          this.state.responses < 5 &&
+          this.state.questionBank.map(
+            ({ question, answers, correct, questionId }) => (
+              <QuestionBox
+                question={question}
+                options={answers}
+                key={questionId}
+                selected={(answer) => this.computeAnswer(answer, correct)}
+              />
+            )
+          )}
+        {this.state.responses === 5 ? (
+          <Result score={this.state.score} playAgain={this.playAgain} />
+        ) : null} */}
+		</div>
+	);
+}
+
+export default App;
